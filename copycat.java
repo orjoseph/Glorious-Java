8ad1bb493e7d6a1d5aa6188238dd08f3f2ea2b99
orjoseph <orjoseph@gmail.com>
2013-03-04 22:48:26 -0500

    CopyCat
    Program Just repeats whatever you enter

---
+++ copycat.java
@@ -0,0 +1,15 @@
+/*Program repeats what ever u wrote in the prompt
+ * uses Scanner to read in the lines u entered
+ */
+import java.util.Scanner;
+public class example01 
+{
+  public static void main(String args[])
+	{
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println(myScanner.nextLine());
+	}
+	
+}
+ 
